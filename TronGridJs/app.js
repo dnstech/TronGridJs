9c506f514a9ba3da16f2cb06a7b3f4276dab8116
Andrew Chisholm <andrewc@dnstech.com.au>
2014-10-13 23:29:56 +1100

    Initial Project

---
+++ TronGridJs/app.js
@@ -0,0 +1,27 @@
+﻿var TronGrid = (function () {
+    function TronGrid(element, options) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('div');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+    TronGrid.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () {
+            return _this.span.innerHTML = new Date().toUTCString();
+        }, 500);
+    };
+
+    TronGrid.prototype.stop = function () {
+        clearTimeout(this.timerToken);
+    };
+    return TronGrid;
+})();
+
+window.onload = function () {
+    var el = document.getElementById('content');
+    var greeter = new TronGrid(el);
+    greeter.start();
+};
+//# sourceMappingURL=app.js.map
---
+++ TronGridJs/app.ts
@@ -0,0 +1,34 @@
+﻿interface ITronGridOptions {
+    primaryOrientation: string;
+    rowSize: (item) => number;
+
+}
+
+class TronGrid {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement, options?: ITronGridOptions) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('div');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new TronGrid(el);
+    greeter.start();
+};
