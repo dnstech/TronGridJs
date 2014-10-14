51dc5a48115fb56a689ed2284cd9bf549afa2c50
Andrew Chisholm <andrewc@dnstech.com.au>
2014-10-15 01:00:35 +1100

    More work on the Grid binding and bounds calculation

--- TronGridJs/trongrid.js
+++ TronGridJs/trongrid.js
@@ -1,115 +1,210 @@
 ﻿/// <reference path="types/knockout.d.ts" />
 /// <reference path="types/jquery.d.ts" />
 
-var TronGridCellRange = (function () {
-    function TronGridCellRange(row, column) {
-        this.row = row;
-        this.column = column;
-        this.isRendered = false;
-    }
-    /** Binds to just the relevant portion of the full two-dimensional cells array */
-    TronGridCellRange.prototype.bind = function (cells) {
-        this.html = '';
-        for (var r = this.row; r++; r < cells.length) {
-            for (var c = this.column; c++; c < cells[r].length) {
-                var item = cells[r][c];
+var TronGrid;
+(function (_TronGrid) {
+    var CellRange = (function () {
+        function CellRange(row, column, measureCell, template) {
+            this.row = row;
+            this.column = column;
+            this.measureCell = measureCell;
+            this.template = template;
+            this.isRendered = false;
+        }
+        /** Binds to just the relevant portion of the full two-dimensional cells array */
+        CellRange.prototype.bind = function (cells) {
+            this.html = '';
+            this.boundingBox.zeroSize();
+            for (var r = this.row; r++; r < cells.length) {
+                for (var c = this.column; c++; c < cells[r].length) {
+                    var item = cells[r][c];
+                    var size = this.measureCell(cells[r][c]);
+                    this.boundingBox.growBy(size);
 
-                // TODO: Add custom cell css class selection here
-                this.html += '<div id="tg_' + r + '_' + c + '" class="cell">' + item + '</div>';
+                    // TODO: Add custom cell css class selection here
+                    this.html += '<div id="tg_' + r + '_' + c + '" class="cell">' + this.cellHtml(item) + '</div>';
+                }
             }
-        }
-    };
-    return TronGridCellRange;
-})();
-
-var TronGridCellRangeViewport = (function () {
-    function TronGridCellRangeViewport() {
-        this.currentRange = null;
-    }
-    TronGridCellRangeViewport.prototype.render = function (range) {
-        if (this.currentRange !== null) {
-            this.currentRange.isRendered = false;
-        }
-
-        this.viewport.style.top = range.y + 'px';
-        this.viewport.style.left = range.x + 'px';
-        this.viewport.style.width = range.width + 'px';
-        this.viewport.style.height = range.height + 'px';
-        this.viewport.innerHTML = range.html;
-        range.isRendered = true;
-        this.currentRange = range;
-    };
-
-    TronGridCellRangeViewport.prototype.add = function (parent) {
-        this.viewport = document.createElement('div');
-        parent.appendChild(this.viewport);
-    };
-    return TronGridCellRangeViewport;
-})();
-
-var TronGrid = (function () {
-    function TronGrid(element) {
-        var _this = this;
-        this.defaultOptions = {
-            orientation: 'rows',
-            cellSize: function (item) {
-                return {
-                    width: 100,
-                    height: 25
-                };
-            },
-            template: function (item) {
-                return null;
-            },
-            data: null
         };
-        this.options = this.defaultOptions;
-        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-            return _this.dispose();
-        });
-    }
-    TronGrid.prototype.update = function (options) {
-        this.options = $.extend(options || {}, this.defaultOptions);
-        this.render();
-    };
 
-    TronGrid.prototype.render = function () {
-        var _this = this;
-        var d = this.options.data;
-        if (ko.isObservable(d)) {
-            this.dataSubscription = d.subscribe(function () {
-                return _this.dataChanged();
-            });
+        CellRange.prototype.cellHtml = function (cell) {
+            var d = ko.renderTemplate(this.template, cell);
+        };
+        return CellRange;
+    })();
+
+    var Rectangle = (function () {
+        function Rectangle(left, top, width, height) {
+            if (typeof width === "undefined") { width = 0; }
+            if (typeof height === "undefined") { height = 0; }
+            this.left = left;
+            this.top = top;
+            this.width = width;
+            this.height = height;
+            this.recalculate();
         }
-    };
+        Rectangle.prototype.intersects = function (other) {
+            return !(this.left > other.right || this.right < other.left || this.top > other.bottom || this.bottom < other.top);
+        };
 
-    TronGrid.prototype.dataChanged = function () {
-    };
+        Rectangle.prototype.zeroSize = function () {
+            this.width = 0;
+            this.height = 0;
+            this.recalculate();
+        };
 
-    TronGrid.prototype.dispose = function () {
-        if (!!this.dataSubscription) {
-            this.dataSubscription.dispose();
-            this.dataSubscription = null;
+        Rectangle.prototype.growBy = function (size) {
+            this.width += size.width;
+            this.height += size.height;
+            this.recalculate();
+        };
+
+        Rectangle.prototype.apply = function (element) {
+            element.style.top = this.top + 'px';
+            element.style.left = this.left + 'px';
+            element.style.bottom = this.bottom + 'px';
+            element.style.right = this.right + 'px';
+        };
+
+        Rectangle.prototype.recalculate = function () {
+            this.right = this.left + this.width;
+            this.bottom = this.top + this.height;
+        };
+        return Rectangle;
+    })();
+
+    /** Represents a chunk of rendered content that can be moved and recycled */
+    var Viewport = (function () {
+        function Viewport() {
+            this.currentRange = null;
         }
-    };
-    return TronGrid;
-})();
+        Viewport.prototype.render = function (range) {
+            if (this.currentRange !== null) {
+                this.currentRange.isRendered = false;
+            }
 
-var TronGridBindingHandler = (function () {
-    function TronGridBindingHandler() {
-    }
-    TronGridBindingHandler.prototype.init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        var grid = new TronGrid(element);
-        $(element).data('trongrid', grid);
-    };
+            if (!range) {
+                this.viewport.innerHTML = '';
+                this.currentRange = null;
+                return;
+            }
 
-    TronGridBindingHandler.prototype.update = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        var o = valueAccessor();
-        var grid = $(element).data('trongrid');
-        grid.update(o);
-    };
-    return TronGridBindingHandler;
-})();
+            this.viewport.innerHTML = range.html;
+            range.isRendered = true;
+            this.currentRange = range;
+        };
 
-ko.bindingHandlers.tronGrid = new TronGridBindingHandler();
+        Viewport.prototype.add = function (parent) {
+            this.viewport = document.createElement('div');
+            parent.appendChild(this.viewport);
+        };
+        return Viewport;
+    })();
+
+    /** The main grid logic, which is attached to the Scroller via the binding handler */
+    var TronGrid = (function () {
+        function TronGrid(scroller) {
+            this.scroller = scroller;
+            this.defaultOptions = {
+                orientation: 'rows',
+                cellSize: function (item) {
+                    return {
+                        width: 100,
+                        height: 25
+                    };
+                },
+                template: function (item) {
+                    return null;
+                },
+                viewportRows: 10,
+                viewportColumns: 10,
+                data: null
+            };
+            this.options = this.defaultOptions;
+            this.registerEventHandlers();
+        }
+        TronGrid.prototype.update = function (options) {
+            this.options = $.extend(options || {}, this.defaultOptions);
+            this.render();
+        };
+
+        TronGrid.prototype.render = function () {
+            ////var rangeIndex = this.scrollBounds.left * this.r;
+        };
+
+        TronGrid.prototype.dataChanged = function () {
+            this.disposeData();
+
+            // TODO: provide more fully functional mapping features for flattening complicated data structures.
+            var updatedCells = ko.unwrap(this.options.data);
+
+            var rangeIndex = 0;
+            for (var r = 0; r += this.options.viewportRows; r < updatedCells.length) {
+                for (var c = 0; c += this.options.viewportColumns; c < updatedCells[r].length) {
+                    var range = this.ranges[rangeIndex];
+                    if (!range) {
+                        range = new CellRange(r, c, this.options.cellSize, this.options.template);
+                        this.ranges[rangeIndex] = range;
+                    }
+
+                    range.bind(updatedCells);
+                }
+            }
+
+            // TODO: Use ko.compareArrays to be more efficient and bind only affected ranges
+            this.cells = updatedCells;
+
+            this.render();
+            this.subscribeData();
+        };
+
+        TronGrid.prototype.subscribeData = function () {
+            var _this = this;
+            var d = this.options.data;
+            if (ko.isObservable(d)) {
+                this.dataSubscription = d.subscribe(function () {
+                    return _this.dataChanged();
+                });
+            }
+        };
+
+        TronGrid.prototype.disposeData = function () {
+            if (!!this.dataSubscription) {
+                this.dataSubscription.dispose();
+                this.dataSubscription = null;
+            }
+        };
+
+        TronGrid.prototype.registerEventHandlers = function () {
+            ko.utils.registerEventHandler(this.scroller, 'resize', this.sizeChanged);
+            ko.utils.registerEventHandler(this.scroller, 'scroll', this.scrollChanged);
+        };
+
+        TronGrid.prototype.sizeChanged = function () {
+        };
+
+        TronGrid.prototype.scrollChanged = function () {
+        };
+        return TronGrid;
+    })();
+
+    var BindingHandler = (function () {
+        function BindingHandler() {
+        }
+        BindingHandler.prototype.init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var grid = new TronGrid(element);
+            ko.utils.domData.set(element, 'trongrid', grid);
+        };
+
+        BindingHandler.prototype.update = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var o = valueAccessor();
+            var grid = ko.utils.domData.get(element, 'trongrid');
+            grid.update(o);
+        };
+        return BindingHandler;
+    })();
+    _TronGrid.BindingHandler = BindingHandler;
+})(TronGrid || (TronGrid = {}));
+
+ko.bindingHandlers.tronGrid = new TronGrid.BindingHandler();
 //# sourceMappingURL=trongrid.js.map
--- TronGridJs/trongrid.ts
+++ TronGridJs/trongrid.ts
@@ -5,120 +5,220 @@ interface KnockoutBindingHandlers {
     tronGrid: KnockoutBindingHandler;
 }
 
-interface ITronGridOptions {
-    orientation: string;
-    cellSize: (item) => {
+
+module TronGrid {
+    interface ISize {
         /** Width in Pixels */
         width: number;
         /** Height in Pixels */
         height: number;
-    };
-    template: (item) => string;
-    data: any;
-}
-
-class TronGridCellRange {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    html: string;
-    isRendered: boolean = false;
-
-    constructor(public row: number, public column: number) {
     }
 
-    /** Binds to just the relevant portion of the full two-dimensional cells array */
-    bind(cells: any[][]) {
-        this.html = '';
-        for (var r = this.row; r++; r < cells.length) {
-            for (var c = this.column; c++; c < cells[r].length) {
-                var item = cells[r][c];
-                // TODO: Add custom cell css class selection here
-                this.html += '<div id="tg_' + r + '_' + c + '" class="cell">' + item + '</div>';
+    interface IOptions {
+        orientation: string;
+        cellSize: (item) => ISize;
+        template: (item) => string;
+        viewportRows: number;
+        viewportColumns: number;
+        data: any;
+    }
+
+    class CellRange {
+        boundingBox: Rectangle;
+        html: string;
+        isRendered: boolean = false;
+
+        constructor(public row: number, public column: number, private measureCell: (cell) => ISize, private template: (cell) => string) {
+        }
+
+        /** Binds to just the relevant portion of the full two-dimensional cells array */
+        bind(cells: any[][]) {
+            this.html = '';
+            this.boundingBox.zeroSize();
+            for (var r = this.row; r++; r < cells.length) {
+                for (var c = this.column; c++; c < cells[r].length) {
+                    var item = cells[r][c];
+                    var size = this.measureCell(cells[r][c]);
+                    this.boundingBox.growBy(size);
+
+                    // TODO: Add custom cell css class selection here
+                    this.html += '<div id="tg_' + r + '_' + c + '" class="cell">' + this.cellHtml(item) + '</div>';
+                }
             }
         }
+
+        cellHtml(cell: any) {
+            var d = ko.renderTemplate(this.template, cell);
+        }
     }
-}
 
-class TronGridCellRangeViewport {
-    viewport: HTMLElement;
-    currentRange: TronGridCellRange = null;
+    class Rectangle {
+        right: number;
+        bottom: number;
 
-    render(range: TronGridCellRange) {
-        if (this.currentRange !== null) {
-            this.currentRange.isRendered = false;
+        constructor(public left: number, public top: number, public width: number = 0, public height: number = 0) {
+            this.recalculate();
         }
 
-        this.viewport.style.top = range.y + 'px';
-        this.viewport.style.left = range.x + 'px';
-        this.viewport.style.width = range.width + 'px';
-        this.viewport.style.height = range.height + 'px';
-        this.viewport.innerHTML = range.html;
-        range.isRendered = true;
-        this.currentRange = range;
+        intersects(other: Rectangle) {
+            return !(this.left > other.right ||
+                     this.right < other.left ||
+                     this.top > other.bottom ||
+                     this.bottom < other.top);
+        }
+
+        zeroSize() {
+            this.width = 0;
+            this.height = 0;
+            this.recalculate();
+        }
+
+        growBy(size: ISize) {
+            this.width += size.width;
+            this.height += size.height;
+            this.recalculate();
+        }
+
+        apply(element: HTMLElement) {
+            element.style.top = this.top + 'px';
+            element.style.left = this.left + 'px';
+            element.style.bottom = this.bottom + 'px';
+            element.style.right = this.right + 'px';
+        }
+
+        private recalculate() {
+            this.right = this.left + this.width;
+            this.bottom = this.top + this.height;
+        }
     }
 
-    add(parent: HTMLElement) {
-        this.viewport = document.createElement('div');
-        parent.appendChild(this.viewport);
-    }
-}
+    /** Represents a chunk of rendered content that can be moved and recycled */
+    class Viewport {
+        viewport: HTMLElement;
+        currentRange: CellRange = null;
 
-class TronGrid {
-    defaultOptions: ITronGridOptions = {
-        orientation: 'rows',
-        cellSize: (item) => {
-            return {
-                width: 100,
-                height: 25
+        render(range: CellRange) {
+            if (this.currentRange !== null) {
+                this.currentRange.isRendered = false;
             }
-        },
-        template: (item) => null,
-        data: null
-    };
-    element: HTMLElement;
-    options: ITronGridOptions = this.defaultOptions;
-    cells: any[][];
-    dataSubscription: KnockoutSubscription;
-    constructor(element: HTMLElement) {
-        ko.utils.domNodeDisposal.addDisposeCallback(element, () => this.dispose());
-    } 
 
-    update(options: ITronGridOptions) {
-        this.options = $.extend(options || {}, this.defaultOptions);
-        this.render();
-    }
+            if (!range) {
+                this.viewport.innerHTML = '';
+                this.currentRange = null;
+                return;
+            }
 
-    render() {
-        var d = this.options.data;
-        if (ko.isObservable(d)) {
-            this.dataSubscription = (<KnockoutObservable<any>>d).subscribe(() => this.dataChanged());
+            this.viewport.innerHTML = range.html;
+            range.isRendered = true;
+            this.currentRange = range;
+        }
+
+        add(parent: HTMLElement) {
+            this.viewport = document.createElement('div');
+            parent.appendChild(this.viewport);
         }
     }
 
-    dataChanged() {
+    /** The main grid logic, which is attached to the Scroller via the binding handler */
+    class TronGrid {
+        defaultOptions: IOptions = {
+            orientation: 'rows',
+            cellSize: (item) => {
+                return {
+                        width: 100,
+                        height: 25
+                    }
+            },
+            template: (item) => null,
+            viewportRows: 10,
+            viewportColumns: 10,
+            data: null
+        };
+
+        options: IOptions = this.defaultOptions;
+        cells: any[][];
+        dataSubscription: KnockoutSubscription;
+        ranges: CellRange[];
+        viewports: Viewport[];
+        scrollBounds: Rectangle;
+
+        constructor(public scroller: HTMLElement) {
+            this.registerEventHandlers();
+        }
+
+        update(options: IOptions) {
+            this.options = $.extend(options || {}, this.defaultOptions);
+            this.render();
+        }
+
+        render() {
+            ////var rangeIndex = this.scrollBounds.left * this.r;
+        }
+
+        dataChanged() {
+            this.disposeData();
+
+            // TODO: provide more fully functional mapping features for flattening complicated data structures.
+            var updatedCells = ko.unwrap(this.options.data);
+            
+            var rangeIndex = 0;
+            for (var r = 0; r += this.options.viewportRows; r < updatedCells.length) {
+                for (var c = 0; c += this.options.viewportColumns; c < updatedCells[r].length) {
+                    var range = this.ranges[rangeIndex];
+                    if (!range) {
+                        range = new CellRange(r, c, this.options.cellSize, this.options.template);
+                        this.ranges[rangeIndex] = range;
+                    }
+
+                    range.bind(updatedCells);
+                }
+            }
+
+            // TODO: Use ko.compareArrays to be more efficient and bind only affected ranges
+            this.cells = updatedCells;
+
+            this.render();
+            this.subscribeData();
+        }
+
+        subscribeData() {
+            var d = this.options.data;
+            if (ko.isObservable(d)) {
+                this.dataSubscription = (<KnockoutObservable<any>>d).subscribe(() => this.dataChanged());
+            }
+        }
+
+        disposeData() {
+            if (!!this.dataSubscription) {
+                this.dataSubscription.dispose();
+                this.dataSubscription = null;
+            }
+        }
+
+        registerEventHandlers() {
+            ko.utils.registerEventHandler(this.scroller, 'resize', this.sizeChanged);
+            ko.utils.registerEventHandler(this.scroller, 'scroll', this.scrollChanged);
+        }
+
+        sizeChanged() {
+        }
+
+        scrollChanged() {
+        }
     }
 
-    dispose() {
-        if (!!this.dataSubscription) {
-            this.dataSubscription.dispose();
-            this.dataSubscription = null;
+    export class BindingHandler implements KnockoutBindingHandler {
+        init(element: HTMLElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) {
+            var grid = new TronGrid(element);
+            ko.utils.domData.set(element, 'trongrid', grid);
+        }
+
+        update(element: any, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) {
+            var o = valueAccessor();
+            var grid: TronGrid = ko.utils.domData.get(element, 'trongrid');
+            grid.update(o);
         }
     }
 }
 
-class TronGridBindingHandler implements KnockoutBindingHandler {
-    init(element: HTMLElement, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) {
-        var grid = new TronGrid(element);
-        $(element).data('trongrid', grid);
-    }
-
-    update(element: any, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) {
-        var o = valueAccessor();
-        var grid:TronGrid = $(element).data('trongrid');
-        grid.update(o);
-    }
-}
-
-ko.bindingHandlers.tronGrid = new TronGridBindingHandler();
+ko.bindingHandlers.tronGrid = new TronGrid.BindingHandler();
